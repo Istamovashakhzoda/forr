a7e324f3d477262345b536de12deed78be96a1c2
Istamovashakhzoda <117287001+Istamovashakhzoda@users.noreply.github.com>
2023-02-20 15:07:31 +0300

    Add files via upload

---
+++ for-math/script.js
@@ -0,0 +1,68 @@
+"use script"
+//1-MASALA
+/*let k=Number(prompt("k"));
+let n=Number(prompt("n"));
+let i=(k*n);
+for ( let i=0; i<10; i++){
+  console.log(i,k,n);
+  }*/
+
+//2-MASALA
+/*let a=Number(prompt("a-son"));
+let b=Number(prompt("b-son"));
+let sum="";
+for(let i=a; i<b; i++){
+  sum=sum+i+"";
+  console.log(i);
+}*/
+
+//3-MASALA
+/*let a=Number(prompt("a-son"));
+let b=Number(prompt("b-son"));
+let sum="";
+//for(let i=a; i<b; i++){
+  sum=sum-i-"";
+  console.log(i);}*/
+
+  //4-MASALA
+  /*let a=Number(prompt("1-kg konfet kiriting"));
+  for(let i=0; i<a;i++){
+  let =(a*i)
+  console.log(i);
+  //console.log(n);
+}*/
+
+//5-MASALA
+/*let a=Number(prompt("1-kg konfet kiriting"));
+  for(let i=a; i<11;i++){
+    console.log(i);
+  let =(i/10+(a*i/10));
+}*/
+
+//6-MASALA
+/*let a=Number(prompt("1-kg konfet kiriting"));
+  for(let i=a; i<11;i++){
+    console.log(i);
+  let =(i/11+(a*i/10));
+}*/
+
+//7-MASALA
+/*let a=Number(prompt("a-son"));
+let b=Number(prompt("b-son"));
+let sum=""
+for(let i=0; i<10; i++){
+  let i=(a,b+1)
+  console.log(i);
+}*/
+
+//8-MASALA
+/*let a=Number(prompt("a-son"));
+let b=Number(prompt("b-son"));
+let sum=""
+for(let i=0; i<10; i++){
+  let i=(a,b*1);
+  console.log(i);
+}*/
+
+//9-MASALA
+
